2cc2edd25756960ac22172ee38f55234a4861563
juroVee <juro.vetrak@gmail.com>
2019-10-21 14:45:04 +0200

    modifying plot size

--- editor/jedit/gui/board.py
+++ editor/jedit/gui/board.py
@@ -1,15 +1,16 @@
 import ipywidgets as widgets
-from ipywidgets import VBox, AppLayout, Output
+from math import floor, ceil
+from ipywidgets import VBox, AppLayout, Output, Button, Layout, GridspecLayout
 from .sliders import freq_slider, range_slider
 from .buttons import color_buttons
 from ..plots import Plot
 
 import matplotlib.pyplot as plt
 
-# def create_expanded_button(description, button_style):
-#     return Button(description=description, button_style=button_style, layout=Layout(height='auto', width='auto'))
+def create_expanded_button(description, button_style):
+    return Button(description=description, button_style=button_style, layout=Layout(height='auto', width='auto'))
 
-# header_button = create_expanded_button('Title', 'success')
+header_button = create_expanded_button('Title', 'success')
 # left_button = create_expanded_button('Left', 'info')
 # center_button = create_expanded_button('Center', 'warning')
 # right_button = create_expanded_button('Right', 'info')
@@ -21,12 +22,27 @@ class JTab:
         self.header, self.footer = header, footer
         self.left_sidebar, self.center, self.right_sidebar = left_sidebar, center, right_sidebar
 
-    def app_layout(self):
+    def _calculate_grid_size(self, plot_width: int, plot_height: int) -> tuple:
+        rows = plot_height + ceil(plot_height / 3)
+        cols = plot_width + ceil(plot_width / 3)
+        mid = ceil((7 * cols) / 10)
+        return rows, cols, mid
+
+    def app_layout(self) -> AppLayout:
         return AppLayout(header=self.header,
           left_sidebar=self.left_sidebar,
           center=self.center,
           right_sidebar=self.right_sidebar,
           footer=self.footer)
+
+    def grid_space(self, plot_width: int, plot_height: int) -> GridspecLayout:
+        rows, cols, mid = self._calculate_grid_size(plot_width, plot_height)
+        print(f'r: {rows}, c: {cols}, m: {mid}')
+        height_px = str(rows * 100) + "px"
+        grid = GridspecLayout(rows, cols, height=height_px)
+        grid[:, :mid] = self.center
+        grid[:, mid:] = self.right_sidebar
+        return grid
 
 
 class Board:
@@ -36,11 +52,11 @@ class Board:
 
     def _init_tabs(self):
         pw = self.plot.get_widget()
-        plt.ioff()
+        pw_size_x, pw_size_y = self.plot.size
         self.tab1 = JTab(center=VBox(children=[pw]), right_sidebar=VBox(children=[freq_slider, range_slider]))
         self.tab2 = JTab(footer=VBox(children=[color_buttons]))
         self.tab3 = JTab(center=VBox())
-        self.tabs = [self.tab1.app_layout(), self.tab2.app_layout(), self.tab3.app_layout()]
+        self.tabs = [self.tab1.grid_space(pw_size_x, pw_size_y), self.tab2.app_layout(), self.tab3.app_layout()]
         self.tab_names = ['Analysis', 'Settings', 'Info']
         self.tab = widgets.Tab(children=self.tabs)
         for i, name in enumerate(self.tab_names):
--- editor/jedit/plots/plot.py
+++ editor/jedit/plots/plot.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt
 from ipywidgets import interactive, Output
-
-def plot_function():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3], [4, 5, 6])
-    fig.show()
+from math import floor, ceil
 
 class Plot:
     """nacita hodnoty grafu nakresleneho uzivatelom
@@ -12,9 +8,27 @@ class Plot:
     def __init__(self, fig, ax):
         self.data = {}
         self.fig, self.ax = fig, ax
+        self.size = (7, 9) # temporary
+        self._resize()
 
-    def get_widget(self):
-        return interactive(plot_function)
+    def _resize(self):
+        x, y = self.size
+        print(f'original: {self.size}')
+        x, y = floor((3 * x) / 4), floor((3 * y) / 4)
+        self.size = x, y
+        print(f'resized: {self.size}')
+
+    def plot_function(self):
+        fig, ax = plt.subplots()
+        size_x, size_y = self.size
+        fig.set_size_inches(size_x, size_y)
+        ax.plot([1, 2, 3], [4, 5, 6])
+        fig.show()
+
+    def get_widget(self) -> interactive:
+        widget = interactive(self.plot_function)
+        widget.layout = {'border': '5px solid black'}
+        return widget
 
 
 
