07e6c94f490b563296c4d79c55aa16851c2f3f78
juroVee <juro.vetrak@gmail.com>
2019-10-21 16:30:14 +0200

    fixed tabs size and plot size

--- editor/jedit/gui/board.py
+++ editor/jedit/gui/board.py
@@ -18,31 +18,25 @@ header_button = create_expanded_button('Title', 'success')
 
 class JTab:
     """Fixed layout - AppLayout (header, footer, left, right sidebar and center"""
-    def __init__(self, header=None, footer=None, left_sidebar=None, center=None, right_sidebar=None):
+    def __init__(self, name=None, header=None, footer=None, left_sidebar=None, center=None, right_sidebar=None):
+        self.name = name
         self.header, self.footer = header, footer
         self.left_sidebar, self.center, self.right_sidebar = left_sidebar, center, right_sidebar
 
-    def _calculate_grid_size(self, plot_width: int, plot_height: int) -> tuple:
-        rows = plot_height + ceil(plot_height / 3)
-        cols = plot_width + ceil(plot_width / 3)
-        mid = ceil((7 * cols) / 10)
-        return rows, cols, mid
-
-    def app_layout(self) -> AppLayout:
+    def app_layout(self) -> AppLayout: # not used yet
         return AppLayout(header=self.header,
           left_sidebar=self.left_sidebar,
           center=self.center,
           right_sidebar=self.right_sidebar,
           footer=self.footer)
 
-    def grid_space(self, plot_width: int, plot_height: int) -> GridspecLayout:
-        rows, cols, mid = self._calculate_grid_size(plot_width, plot_height)
-        print(f'r: {rows}, c: {cols}, m: {mid}')
-        height_px = str(rows * 100) + "px"
-        grid = GridspecLayout(rows, cols, height=height_px)
-        grid[:, :mid] = self.center
-        grid[:, mid:] = self.right_sidebar
+    def grid_space(self) -> GridspecLayout:
+        grid = GridspecLayout(10, 10, height='600px')
+        grid[:, :7] = self.center
+        grid[:, 7:] = self.right_sidebar
+        #grid[:2, :7] = header_button
         return grid
+
 
 
 class Board:
@@ -52,15 +46,23 @@ class Board:
 
     def _init_tabs(self):
         pw = self.plot.get_widget()
-        pw_size_x, pw_size_y = self.plot.size
-        self.tab1 = JTab(center=VBox(children=[pw]), right_sidebar=VBox(children=[freq_slider, range_slider]))
-        self.tab2 = JTab(footer=VBox(children=[color_buttons]))
-        self.tab3 = JTab(center=VBox())
-        self.tabs = [self.tab1.grid_space(pw_size_x, pw_size_y), self.tab2.app_layout(), self.tab3.app_layout()]
-        self.tab_names = ['Analysis', 'Settings', 'Info']
-        self.tab = widgets.Tab(children=self.tabs)
-        for i, name in enumerate(self.tab_names):
-            self.tab.set_title(i, name)
+        tabs = [
+            JTab(name='Analysis',
+                 center=VBox(children=[pw]),
+                 right_sidebar=VBox(children=[freq_slider] * 5)
+                 ),
+            JTab(name='Settings',
+                 center=VBox(children=[color_buttons]),
+                 right_sidebar=VBox()
+                 ),
+            JTab(name='Info',
+                 center=VBox(),
+                 right_sidebar=VBox()
+                 )
+        ]
+        self.tab = widgets.Tab(children=[t.grid_space() for t in tabs])
+        for i, tab in enumerate(tabs):
+            self.tab.set_title(i, tab.name)
 
     def get(self):
         return VBox(children=[self.tab])
--- editor/jedit/plots/plot.py
+++ editor/jedit/plots/plot.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from .maux import *
 from ipywidgets import interactive, Output
+from IPython.display import display
 from math import floor, ceil
+
+def f(X):
+    return 2 * X ** 4 - 7 * X ** 3 + 5 * X ** 2 + 3 * X - 2
 
 class Plot:
     """nacita hodnoty grafu nakresleneho uzivatelom
@@ -9,25 +15,34 @@ class Plot:
         self.data = {}
         self.fig, self.ax = fig, ax
         self.size = (7, 9) # temporary
+        self.interactive_fields = False
         self._resize()
 
     def _resize(self):
+        # default DPI in Matplotlib = 100
+        grid_height = 400 if self.interactive_fields else 500
         x, y = self.size
         print(f'original: {self.size}')
-        x, y = floor((3 * x) / 4), floor((3 * y) / 4)
-        self.size = x, y
+        factor = (y * 100) / grid_height
+        self.size = x / factor, y / factor
         print(f'resized: {self.size}')
 
     def plot_function(self):
         fig, ax = plt.subplots()
         size_x, size_y = self.size
-        fig.set_size_inches(size_x, size_y)
-        ax.plot([1, 2, 3], [4, 5, 6])
+        fig.set_size_inches(6, 5)
+        X = np.linspace(-0.70, 2.20, 29 * 100 + 1)  # výber hodnôt nezávislej premennej pre zaujímavú časť grafu
+        Y = f(X)
+        init_subplot(ax)
+        ax.set_title(r"Graf funkcie $y = 2x^4-7x^3+5x^2+3x-2$")  # pomenovanie diagramu
+        ax.set_aspect('equal')
+        plt.ioff()
+        ax.plot(X, Y)
         fig.show()
 
     def get_widget(self) -> interactive:
         widget = interactive(self.plot_function)
-        widget.layout = {'border': '5px solid black'}
+        #widget.layout = {'border': '5px solid black'}
         return widget
 
 
