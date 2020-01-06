b8d306c70cb79b91f005883365d80178b1f01abc
juroVee <juro.vetrak@gmail.com>
2020-01-06 17:50:46 +0100

    log on webpage changed to weekly

--- editor/jedit/calculations/calculator.py
+++ editor/jedit/calculations/calculator.py
@@ -1,6 +1,5 @@
 # external modules
-from scipy.misc import derivative
-from scipy.optimize import newton, brentq, bisect
+from scipy import misc, optimize
 import numpy as np
 
 # package-level modules
@@ -15,7 +14,7 @@ class Calculator:
 
     def derive(self, X, n):
         order = n + 1 if n % 2 == 0 else n + 2
-        return X, derivative(self.f, X, dx=0.001, n=n, order=order)
+        return X, misc.derivative(self.f, X, dx=0.001, n=n, order=order)
 
     def _find_sign_change(self, X):
         return np.where(np.diff(np.signbit(self.f(X))))[0]
@@ -25,8 +24,8 @@ class Calculator:
         sign_changes = zip(indexes, np.add(1, indexes))
         for i, j in sign_changes:
             if method == 'newton':
-                yield newton(self.f, X[i])
+                yield optimize.newton(self.f, X[i])
             elif method == 'brentq':
-                yield brentq(self.f, X[i], X[j])
+                yield optimize.brentq(self.f, X[i], X[j])
             elif method == 'bisect':
-                yield bisect(self.f, X[i], X[j])
+                yield optimize.bisect(self.f, X[i], X[j])
